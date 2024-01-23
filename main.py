94818a527750fca5867e1f9fd7704b04c28f20f9
GitaWardani <157507159+GitaWardani@users.noreply.github.com>
2024-01-23 23:02:31 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,37 +6,37 @@ from typing import List
 
 app = FastAPI()
 
-# Database untuk menyimpan data mobil
+
 cars_db = []
 
-# Model Pydantic untuk Car
+
 class Car(BaseModel):
     brand: str
     model: str
     year: int
     color: str
 
-# Operasi CRUD
 
-# Create Car
+
+
 @app.post("/cars/", response_model=Car)
 def create_car(car: Car):
     cars_db.append(car)
     return car
 
-# Read All Cars
+
 @app.get("/cars/", response_model=List[Car])
 def read_cars():
     return cars_db
 
-# Read Single Car
+
 @app.get("/cars/{car_id}", response_model=Car)
 def read_car(car_id: int):
     if car_id < 0 or car_id >= len(cars_db):
         raise HTTPException(status_code=404, detail="Car not found")
     return cars_db[car_id]
 
-# Update Car
+
 @app.put("/cars/{car_id}", response_model=Car)
 def update_car(car_id: int, updated_car: Car):
     if car_id < 0 or car_id >= len(cars_db):
@@ -50,7 +50,6 @@ def update_car(car_id: int, updated_car: Car):
     
     return current_car
 
-# Delete Car
 @app.delete("/cars/{car_id}", response_model=Car)
 def delete_car(car_id: int):
     if car_id < 0 or car_id >= len(cars_db):
@@ -59,10 +58,10 @@ def delete_car(car_id: int):
     deleted_car = cars_db.pop(car_id)
     return deleted_car
 
-# Serve files from the "static" directory
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# HTML untuk antarmuka pengguna
+
 html_content = """
 <!DOCTYPE html>
 <html>
@@ -147,8 +146,6 @@ html_content = """
 </body>
 </html>
 """
-
-# Endpoint untuk halaman antarmuka pengguna
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     return HTMLResponse(content=html_content, status_code=200)
